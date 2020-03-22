df69d7da757028ebc3740575c2fd262f6eb05903
Martin <cph-ms782@cphbusiness.dk>
2020-03-22 12:18:25 +0100

    remove compile script

--- Login_strategies/md-til-pdf.sh
+++
@@ -1,5 +0,0 @@
-prettier --check "*.md"
-prettier --write "*.md"
-mdtodoc *.md --join
-mdtodoc MERGED.md --layout "page" --theme "github" --highlight-style "atom-one-light"
-html2pdf MERGED.html README.pdf
